57f77234283d034fb915c2a7050f0aaa87466b96
Manish Kumar <87manishkumar@gmail.com>
2021-08-19 10:03:56 +0530

    ag-grid implementation

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -1,11 +1,21 @@
 import { Container } from "@material-ui/core";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import { makeStyles } from "@material-ui/core/styles";
+import {
+  createTheme,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core/styles";
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRouterOutlet from "./app-router.component";
 import { AppHeader, LeftNavigation } from "./shared/components";
 import { useAuthState } from "./shared/hooks";
+
+const appTheme = createTheme({
+  palette: {
+    type: "light",
+  },
+});
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -36,19 +46,23 @@ function App() {
 
   return (
     <Router>
-      <div className={classes.root}>
-        <CssBaseline />
-        {user && <AppHeader open={open} handleDrawerOpen={handleDrawerOpen} />}
-        {user && (
-          <LeftNavigation open={open} handleDrawerClose={handleDrawerClose} />
-        )}
-        <main className={classes.content}>
-          <div className={classes.appBarSpacer} />
-          <Container maxWidth="xl" className={classes.container}>
-            <AppRouterOutlet />
-          </Container>
-        </main>
-      </div>
+      <ThemeProvider theme={appTheme}>
+        <div className={classes.root}>
+          <CssBaseline />
+          {user && (
+            <AppHeader open={open} handleDrawerOpen={handleDrawerOpen} />
+          )}
+          {user && (
+            <LeftNavigation open={open} handleDrawerClose={handleDrawerClose} />
+          )}
+          <main className={classes.content}>
+            <div className={classes.appBarSpacer} />
+            <Container maxWidth="xl" className={classes.container}>
+              <AppRouterOutlet />
+            </Container>
+          </main>
+        </div>
+      </ThemeProvider>
     </Router>
   );
 }
--- src/app/dashboard/dashboard.component.tsx
+++ src/app/dashboard/dashboard.component.tsx
@@ -1,7 +1,108 @@
+import { makeStyles } from "@material-ui/core";
+import { GridOptions, GridReadyEvent } from "ag-grid-community";
+import { AgGridReact } from "ag-grid-react";
 import React from "react";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "calc(100vh - 140px)",
+  },
+}));
+
 function Dashboard() {
-  return <div className="dashboard">Dashboard...</div>;
+  const classes = useStyles();
+  const [rowData, setRowData] = React.useState([]);
+  const [gridApi, setGridApi] = React.useState(null);
+  const [gridColumnApi, setGridColumnApi] = React.useState(null);
+
+  const onGridReady = (params: GridReadyEvent) => {
+    setGridApi(params.api);
+    setGridColumnApi(params.columnApi);
+
+    const updateData = (data: React.SetStateAction<any[]>) => {
+      setRowData(data);
+    };
+
+    fetch("https://www.ag-grid.com/example-assets/olympic-winners.json")
+      .then((resp) => resp.json())
+      .then((data) => updateData(data));
+  };
+
+  const gridOptions: GridOptions = {
+    defaultColDef: {
+      flex: 1,
+      sortable: true,
+      editable: true,
+      filter: "agTextColumnFilter",
+      floatingFilter: false,
+      resizable: true,
+    },
+    defaultColGroupDef: {
+      marryChildren: true,
+      children: [],
+    },
+    columnTypes: {
+      numberColumn: { width: 130, filter: "agNumberColumnFilter" },
+      medalColumn: { width: 100, columnGroupShow: "open", filter: false },
+      nonEditableColumn: { editable: false },
+      dateColumn: {
+        // specify we want to use the date filter
+        filter: false,
+      },
+    },
+    columnDefs: [
+      {
+        headerName: "Athlete",
+        field: "athlete",
+      },
+      {
+        headerName: "Sport",
+        field: "sport",
+      },
+      {
+        headerName: "Age",
+        field: "age",
+        type: "numberColumn",
+      },
+      { headerName: "Year", field: "year", type: "numberColumn" },
+      {
+        headerName: "Date",
+        field: "date",
+        type: ["dateColumn", "nonEditableColumn"],
+        width: 220,
+      },
+      {
+        headerName: "Gold",
+        field: "gold",
+        type: "medalColumn",
+      },
+      {
+        headerName: "Silver",
+        field: "silver",
+        type: "medalColumn",
+      },
+      {
+        headerName: "Bronze",
+        field: "bronze",
+        type: "medalColumn",
+      },
+      {
+        headerName: "Total",
+        field: "total",
+        type: "medalColumn",
+      },
+    ],
+  };
+
+  return (
+    <div className={`ag-theme-alpine ${classes.root}`}>
+      <AgGridReact
+        rowData={rowData}
+        gridOptions={gridOptions}
+        onGridReady={onGridReady}
+      ></AgGridReact>
+    </div>
+  );
 }
 
 export default Dashboard;
